0864f53f9017ee60e47a1e9fba16038056651cc7
jgrisafe <jgrisafe@gmail.com>
2018-05-13 22:19:55 -0700

    remove log

--- server/controllers/views-controller.js
+++ server/controllers/views-controller.js
@@ -1,12 +1,8 @@
 const express  = require('express');
 const router = express.Router();
 
-// home route for demonstrating user auth
-router.get('/', (req, res) => {
+router.get('/', (req, res) => res.render('home', { user: req.user }));
 
-  console.log(req.cookies) // eslint-disable-line no-console
-  res.render('home', { user: req.user })
-});
 router.get('/register', (req, res) => res.render('home', { user: req.user }));
 
 module.exports = router;
